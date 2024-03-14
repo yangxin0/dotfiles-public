aa49456ec5ff58ebc9a4d4ffca7892af350e7558
Xin Yang <yangxin0@outlook.com>
2024-03-14 16:27:17 +0800

    Add nvim config

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        config =function()
+            vim.cmd.colorscheme("onedark")
+        end
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            options = {
+                icons_enabled = false,
+                theme = "onedark",
+                component_separators = "|",
+                section_separators = ""
+            }
+        }
+    }
+}
