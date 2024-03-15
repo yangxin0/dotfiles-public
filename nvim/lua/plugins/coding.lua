411cf821905047621752488d2a09f98a1ac67cb9
Xin Yang <yangxin0@outlook.com>
2024-03-15 16:55:42 +0800

    Add(nvim) nvim-cmp complete plugin

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -3,7 +3,30 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-cmdline",
-    "hrsh7th/nvim-cmp",
+    {
+        "hrsh7th/nvim-cmp",
+        config = function(_, opts)
+            local cmp = require("cmp")
+            opts.mapping = cmp.mapping.preset.insert({
+                ["<CR>"] = cmp.mapping.confirm({
+                    behavior = cmp.ConfirmBehavior.Replace,
+                    select = true
+                }),
+                ["<Tab>"] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    else
+                        fallback()
+                    end
+                end, { "i", "s" })
+            })
+            opts.sources = {
+                { name = "nvim_lsp" },
+                { name = "buffer" }
+            }
+            cmp.setup(opts)
+        end
+    },
     {
         "windwp/nvim-autopairs",
         config = function(_, _)
