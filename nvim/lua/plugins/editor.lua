0896ebc9f0b6ab249c3909cd250b9f8698fb9e62
Xin Yang <yangxin0@outlook.com>
2024-03-19 11:29:15 +0800

    Fix(nvim) bad telescope module name

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -26,14 +26,14 @@ return {
         }
     },
     {
-        "telescope.nvim",
+        "nvim-telescope/telescope.nvim",
         dependencies = {
             "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-file-browser.nvim",
             {
                 "nvim-telescope/telescope-fzf-native.nvim",
                 build = "make"
-            },
-            "nvim-telescope/telescope-file-browser.nvim"
+            }
         },
         keys = {
             {
