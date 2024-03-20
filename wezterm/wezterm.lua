5f16c0b18f5191e96c5762344fd3f3d300b5dba2
Xin Yang <yangxin0@outlook.com>
2024-03-20 09:53:20 +0800

    Update(wezterm): close window withtout confirmation

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,4 +3,5 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("Monaco")
 config.font_size = 15.0
 config.audible_bell = "Disabled"
+config.window_close_confirmation = "NeverPrompt"
 return config
