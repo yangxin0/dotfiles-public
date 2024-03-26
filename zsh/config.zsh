0ad26c5067b3b9544c98b7af657114ba79769f59
Xin Yang <yangxin0@outlook.com>
2024-03-26 16:47:37 +0800

    Add(zsh): conda source function

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -14,9 +14,28 @@ export GOPROXY="https://goproxy.cn"
 eval "$(fzf --zsh)"
 export FZF_DEFAULT_OPTS="--height 100% --layout=reverse"
 
-function print-256-colors () {
+function print-256-colors()
+{
     for i in {0..255}
     do
         print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'}
     done
 }
+
+function source-conda()
+{
+    FORGE=$HOME/miniforge3
+    CONDA="${FORGE}/bin/conda"
+    __conda_setup="$($CONDA 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "${FORGE}/etc/profile.d/conda.sh" ]; then
+            . "${FORGE}/etc/profile.d/conda.sh"
+        else
+            export PATH="${FORGE}/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+}
+
