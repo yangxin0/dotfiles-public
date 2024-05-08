e60a789f68ae6be9728ca632d8b835cc79346c67
Xin Yang <yangxin0@outlook.com>
2024-05-08 19:56:36 +0800

    Update(nvim) keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,7 @@ local map = vim.keymap.set
 map({"n", "v"}, ",,", "<Plug>NERDCommenterToggle")
 map({"n", "v"}, ",y", "<Plug>NERDCommenterYank")
 map("n", "<leader>d", vim.cmd.Ex)
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
