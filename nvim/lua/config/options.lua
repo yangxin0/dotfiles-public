702f5d6ad54047ee091a866eb18bc7f62ea92aec
Xin Yang <yangxin0@outlook.com>
2024-05-08 19:48:19 +0800

    Update(nvim) global options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,15 +2,24 @@ vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
 
+vim.opt.guicursor = ""
+
 vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 vim.opt.title = true
 vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.hlsearch = true
-vim.opt.expandtab = true
 vim.opt.smarttab = true
 vim.opt.breakindent = true
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.wrap = false
