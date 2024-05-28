decbd5c8f150f7a35f7720b853ac68d69d47c36e
Xin Yang <yangxin0@outlook.com>
2024-05-28 11:26:25 +0800

    Update(nvim): Keymap set with description

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,11 +2,11 @@ local map = vim.keymap.set
 
 map({"n", "v"}, ",,", "<Plug>NERDCommenterToggle")
 map({"n", "v"}, ",y", "<Plug>NERDCommenterYank")
-map("n", "<leader>d", vim.cmd.Ex)
+map("n", "<leader>d", vim.cmd.Ex, { desc = "File explorer" })
 map("v", "J", ":m '>+1<CR>gv=gv")
 map("v", "K", ":m '<-2<CR>gv=gv")
 map("n", "<C-d>", "<C-d>zz")
 map("n", "<C-u>", "<C-u>zz")
 map("i", "<C-c>", "<Esc>")
 map("n", "<C-l>", "zz")
-map("n", "K", vim.lsp.buf.hover)
+map("n", "K", vim.lsp.buf.hover, { desc = "Show hover" })
