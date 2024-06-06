0141c758a6dffe39ed5a56cf83802489ed482a22
Xin Yang <yangxin0@outlook.com>
2024-06-06 10:11:09 +0800

    Revert "Update(term): navigate code by mouse"
    This reverts commit 3e69ad518e59f0da8b5ba0b4097ee77a0773bef4.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-
 config.font = wezterm.font("Monaco")
 config.font_size = 15.0
 config.audible_bell = "Disabled"
 config.window_close_confirmation = "NeverPrompt"
-
-config.mouse_bindings = {
-    {
-        event = { Up = { streak = 1, button = "Left" } },
-        mods = "CTRL",
-        action = wezterm.action({ SendKey = { key = "]", mods = "CTRL" } })
-    },
-    {
-        event = { Up = { streak = 1, button = "Right" } },
-        mods = "CTRL",
-        action = wezterm.action({ SendKey = { key = "o", mods = "CTRL" } })
-    }
-}
-
 return config
