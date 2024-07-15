5669a526d12130aee527026cfccd4075ee345a55
Xin Yang <yangxin0@outlook.com>
2024-07-15 18:18:27 +0800

    Updaste(wez): copy and paste
    - copy with selection
    - paste with right click

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,26 @@
 local wezterm = require("wezterm")
+local action = wezterm.action
 local config = wezterm.config_builder()
+
 config.font = wezterm.font("Monaco")
 config.font_size = 15.0
 config.audible_bell = "Disabled"
 config.window_close_confirmation = "NeverPrompt"
+
+config.mouse_bindings = {
+    {
+        event = { Down = { streak = 1, button = "Right" } },
+        mods = "NONE",
+        action = wezterm.action_callback(function(window, pane)
+            local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+            if has_selection then
+                window:perform_action(action.CopyTo("ClipboardAndPrimarySelection"), pane)
+                window:perform_action(action.ClearSelection, pane)
+            else
+                window:perform_action(action({ PasteFrom = "Clipboard" }), pane)
+            end
+        end),
+    },
+}
+
 return config
