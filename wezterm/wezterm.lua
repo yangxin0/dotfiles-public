1e7d1280c654548bc43baaec35980407165c7dad
Xin Yang <yangxin0@outlook.com>
2024-07-24 20:32:34 +0800

    wezterm: Use consolas on Linux

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,27 @@ local wezterm = require("wezterm")
 local action = wezterm.action
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Monaco")
-config.font_size = 15.0
+local function target_os()
+   local target = wezterm.target_triple
+   if target:find("linux") then
+        return "linux"
+    elseif target:find("darwin") then
+        return "apple"
+    else
+        return "windows"
+    end
+end
+
+-- Font
+if target_os() == "apple" then
+    config.font = wezterm.font("Monaco")
+    config.font_size = 15.0
+else
+    -- consolas font is designed by Microsoft
+    config.font = wezterm.font("consolas")
+    config.font_size = 13.0
+end
+
 config.audible_bell = "Disabled"
 config.window_close_confirmation = "NeverPrompt"
 
